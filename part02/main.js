11f230dd3ab7ad66b477aae83fa23716b3092981
sagor4202 <erabbihasan@gmail.com>
2025-09-03 12:24:58 +0600

    today task is complete

---
+++ part02/main.js
@@ -0,0 +1,10 @@
+function myFun() {
+    let a = document.getElementById('myCat');
+
+    a.innerHTML = 'i am change ha ha ha '
+    a.style.color = 'white';
+    a.style.backgroundColor = 'black';
+    a.style.border = ' 5px solid red';
+    a.style.fontSize = ' 20px';
+    a.style.padding = ' 10px';
+};
---
+++ part02/test.js
@@ -0,0 +1,3 @@
+function myfun2(){
+    window.alert(10+2)
+}
