962ad06cc446b44f092d7a0a6ccd30cc223eadd6
sagor4202 <erabbihasan@gmail.com>
2025-09-04 09:10:39 +0600

    new part

---
+++ part03/main.js
@@ -0,0 +1,23 @@
+// function name(p1, p2, p3) {
+//     return ( 10 + 20 + 15)
+// }
+// console.log (name(
+// ))
+
+// Before Arrow:
+// let myFun = function(a,b) {return a * b}
+// console.log(myFun(1,2));
+
+// // With Arrow
+// let myFun = (a,b) => a + b;
+
+// console.log(myFun(10,30));
+
+
+
+function myFun() {
+    let carName = "BMW";
+    let text = "inside " + typeof carName + " " + carName + " " +"excute please";
+    return text
+}
+console.log(myFun());
