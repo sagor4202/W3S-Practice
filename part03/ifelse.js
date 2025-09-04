70965865746dfe00c33963a1cda9ca1443e389f9
sagor4202 <erabbihasan@gmail.com>
2025-09-04 15:27:21 +0600

    practice if else

---
+++ part03/ifelse.js
@@ -0,0 +1,154 @@
+// ! Make a "Good day" greeting if the hour is less than 18:00:
+
+// if (new Date().getHours() < 18 ){
+//     console.log("good day");
+// }
+// else {
+//     console.log("wrong");
+    
+// }
+
+
+//! if the hour is less than 18, create a "Good day" greeting, otherwise "Good evening":
+
+// let hour = 19;
+
+// if (hour < 18 ) {
+//     console.log("good day");
+    
+// }
+// else {
+//     console.log("Good evering");
+    
+// }
+
+
+
+// const hour = new Date().getHours(); 
+// let greeting;
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// document.getElementById('demo2').innerHTML = greeting;
+
+// const hour = new Date().getHours(); 
+// let greeting;
+// if (hour < 11) {
+//   greeting = "Good day";
+// } 
+
+// else if (hour < 18){
+//     greeting = "good morning";
+// }
+
+// else{
+//   greeting = "Good evening";
+// }
+
+// document.getElementById('demo2').innerHTML = greeting;
+
+// const hour = new Date().getHours()
+// let greeting;
+// if (hour < 12 ){
+//     greeting = ("Good Morning")
+// }
+// else if ( hour < 18 ){
+//     greeting = ("Good Afternoon");
+// }
+// else {
+//     greeting = ("Good Evening");
+    
+// }
+
+// document.getElementById('demo2').innerHTML = greeting;
+
+
+// let marks = 32;
+
+// if (marks >= 80 ){
+//     console.log("A+");
+// }
+
+// else if (marks >= 70){
+//     console.log("A");  
+// }
+
+// else if (marks >= 60){
+//     console.log("A-");  
+// }
+
+// else if (marks >= 50){
+//     console.log("B");  
+// }
+
+// else if (marks >= 40){
+//     console.log("C");  
+// }
+
+// else if (marks >= 33){
+//     console.log("D");  
+// }
+
+// else {
+//     console.log("F");
+    
+// }
+
+// let marks = 85;
+// let grade;
+
+// if (marks >= 80 ){
+//      grade = "A+";
+// }
+// else if ( marks >= 70 ){
+//      grade = "A";
+// }
+
+// else if ( marks >= 60 ){
+//      grade = "A-";
+// }
+
+// else if ( marks >= 50 ){
+//      grade = "B";
+// }
+
+// else if ( marks >= 40 ){
+//      grade = "C";
+// }
+
+// else if ( marks >= 33 ){
+//      grade = "D";
+// }
+
+// else {
+//     grade = "Fail 🚩"
+// }
+
+// document.getElementById('demo2').innerHTML =  "Your Grade: " + " " + grade;
+
+
+// !Temperature Checker
+
+// const temperaTure = 60;
+// let massage;
+
+// if ( temperaTure >= 35 ){
+//     massage = "Very Hot 🔥"
+// }
+
+// else if (temperaTure >= 25){
+//     massage = "Hot ☀️"
+// }
+
+// else if (temperaTure >= 10){
+//     massage = "Cool Weather 🌤️"
+// }
+
+// else {
+//     massage = "It's Cold 🥶"
+// }
+
+// document.getElementById('demo2').innerHTML = massage
