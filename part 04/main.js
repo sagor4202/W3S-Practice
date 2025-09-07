a6706f63f01a32b0d52e8a3baf22c005df863613
sagor4202 <erabbihasan@gmail.com>
2025-09-07 11:52:46 +0600

    loops basic practice

---
+++ part 04/main.js
@@ -0,0 +1,70 @@
+// !Loop Type	Description
+// for	     = Iterates over values and expressions
+// while     = Iterates over a condition
+// do...while= Iterates over a condition
+// for...in  =	Iterates over the properties of an Object
+// for...of  =	Iterates over array like objects
+// forEach() = Iterates over each element in an Array
+
+// !For loops 
+// for (let i = 0; i <= 5; i++){
+//     console.log(i);
+// }
+
+// let massage = "";
+// for ( let i = 0; i <=5; i++){
+//     massage += "The number is " + i + "<br>";
+// }
+
+// document.getElementById('demo').innerHTML = massage;
+
+
+// let fruits = ["Apple", "Mango", "Banana"];
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log("This is fruits list " + fruits[i]);
+    
+// }
+
+// let sum = 0;
+// for ( let i = 1; i <= 10; i++) {
+//     sum +=i
+// }
+
+// console.log(sum);
+
+
+// !while Loop
+
+// let i = 0;
+// while (i < 5 ){
+//     console.log(i);
+//     i++
+// }
+
+
+// 2. even number প্রিন্ট
+// let i = 2
+// while (i <= 100 ) {
+//     console.log(i);
+//     i += 2; 
+// }
+
+
+
+// 3. array traverse
+// let numbers = [3, 6, 9, 12];
+// let j = 0;
+// while (j < numbers.length) {
+//   console.log(numbers[j]);
+//   j++;
+// }
+
+
+//! do loops 
+
+a = 0;
+
+do {
+    console.log(a);
+    a++
+} while ( a <= 5)
